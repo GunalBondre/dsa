44f9fd54614b82647491d2e6af9c4e03236745b7
Gunal Bondre <68460418+GunalBondre@users.noreply.github.com>
2025-07-02 11:22:17 +0530

    valid bst

---
+++ patterns/treeTraversal/validateBST.js
@@ -0,0 +1,24 @@
+const bSearchTree = require('../../data structures/binary Search Tree/BST');
+
+const validateBST = (root, min = -Infinity, max = Infinity) => {
+	if (!root) return true;
+
+	if (root.val <= min || root.val >= max) return false;
+
+	return (
+		validateBST(root.left, min, root.val),
+		validateBST(root.left, root.right, max)
+	);
+};
+
+const bst = new bSearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(2);
+bst.insert(7);
+bst.insert(6);
+bst.insert(8);
+bst.insert(15);
+bst.insert(12);
+bst.insert(17);
+console.log(validateBST(bst.root));

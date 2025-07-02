b62bc28498a81dc8afc02d926f3c93750f546c8e
Gunal Bondre <68460418+GunalBondre@users.noreply.github.com>
2025-07-02 10:54:18 +0530

    FinsMinVal

---
+++ patterns/treeTraversal/findMinVal.js
@@ -0,0 +1,23 @@
+const bSearchTree = require('../../data structures/binary Search Tree/BST');
+
+const findMinVal = (root) => {
+	if (root === null) return 0;
+
+	while (root.left !== null) {
+		root = root.left;
+	}
+
+	return root.val;
+};
+
+const bst = new bSearchTree();
+bst.insert(10);
+bst.insert(5);
+bst.insert(2);
+bst.insert(7);
+bst.insert(6);
+bst.insert(8);
+bst.insert(15);
+bst.insert(12);
+bst.insert(17);
+console.log(findMinVal(bst.root));
